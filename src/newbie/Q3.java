871a8a2d7e3031d59cbc5799a5f5f11950967f94
hayun <qkrgkdbs@ajou.ac.kr>
2024-11-23 19:46:24 +0900

    사칙연산

---
+++ src/newbie/Q3.java
@@ -0,0 +1,11 @@
+package newbie;
+//두 수의 합
+public class Q3 {
+  public static void main(String[] args) {
+    int answer = -1;
+    int num1 = 44;
+    int num2 = 33;
+    answer = num1 + num2;
+    System.out.println(answer);
+  }
+}
