e9648465553defe15623de68ef417896f401df47
hayun <qkrgkdbs@ajou.ac.kr>
2024-11-23 19:41:19 +0900

    2번

--- src/newbie/Q2.java
+++ src/newbie/Q2.java
@@ -1,11 +1,11 @@
 package newbie;
-// 두 수의 차
-public class Q1 {
+// 두 수의 곱
+public class Q2 {
   public static void main(String[] args) {
     int answer = 0;
-    int num1 = 2;
-    int num2 = 3;
-    answer = num1 - num2;
+    int num1 = 1;
+    int num2 = 2;
+    answer = num1 * num2;
     System.out.println(answer);
   }
 }
